43d08ba3ef2f0292f7d67f11a4d81b41be6b01b1
Youssouf <bediwy.dada@outlook.fr>
2026-09-12 18:31:12 +0200

    Update Pydantic configuration

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class VehicleCreate(BaseModel):
@@ -14,5 +14,4 @@ class VehicleCreate(BaseModel):
 class VehicleResponse(VehicleCreate):
     id: int
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
